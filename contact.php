91567097842d21e0a6b40ca1ac6a3e1ca7af4ee2
Thierry Petijtean <tpetitjean@mborijnland.nl>
2026-03-11 12:49:28 +0100

    formulier gemaakt

---
+++ contact.php
@@ -0,0 +1,9 @@
+<form action="post" method="post">
+    <label for="name">Naam:</label>
+    <input type="text" id="name" name="name"><br><br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email"><br><br>
+    <label for="message">Bericht:</label><br>
+    <textarea id="message" name="message" rows="4" cols="50"></textarea><br><br>
+    <input type="submit" value="Verzenden">
+    </form>
